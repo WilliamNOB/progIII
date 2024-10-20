1b98369a9a6247261521e40290633ab9761d940a
WilliamNOB <nanoitz98@gmail.com>
2024-10-20 17:16:06 -0500

    implementacion 2FA

--- main.py
+++ main.py
@@ -39,6 +39,86 @@ def send_email():
     return jsonify(data)
 
 
+@app.route("/send_2FAC", methods=["POST"])
+def secondFactor():
+    try:
+        body = request.get_json()
+        user_email = body["email"]
+        token2FA = body["token2FA"]
+        template = (
+    '<!DOCTYPE html>'
+    '<html lang="es">'
+    '<head>'
+    '    <meta charset="UTF-8">'
+    '    <meta name="viewport" content="width=device-width, initial-scale=1.0">'
+    '    <title>Código de verificación</title>'
+    '    <style>'
+    '        body {'
+    '            font-family: Arial, sans-serif;'
+    '            background-color: #f4f4f4;'
+    '            color: #333;'
+    '            padding: 20px;'
+    '        }'
+    '        .container {'
+    '            max-width: 600px;'
+    '            margin: 50px auto;'
+    '            background-color: #fff;'
+    '            padding: 20px;'
+    '            border-radius: 8px;'
+    '            box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);'
+    '        }'
+    '        h3 {'
+    '            color: #007bff;'
+    '            border-bottom: 2px solid #007bff;'
+    '            padding-bottom: 10px;'
+    '        }'
+    '        p {'
+    '            font-size: 18px;'
+    '        }'
+    '        strong {'
+    '            font-weight: bold;'
+    '            color: #007bff;'
+    '        }'
+    '    </style>'
+    '</head>'
+    '<body>'
+    '    <div class="container">'
+    '        <h3>Tu código está listo.</h3>'
+    f'        <p>Tu código es: <strong>{token2FA}</strong></p>'
+    '    </div>'
+    '</body>'
+    '</html>'
+)
+
+
+        message = {
+            "senderAddress": os.environ.get("SENDER_ADDRESS"),
+            "recipients": {
+                "to": [{"address": user_email}],
+            },
+            "content": {
+                "subject": "Código de autenticación",
+                "plainText": "Solicitaste un código de inicio de sesión.",
+                "html": template,
+            },
+        }
+
+        poller = client.begin_send(message)
+        result = poller.result()
+        return jsonify({"message": "Email enviado correctamente", "body": result})
+
+    except Exception as e:
+        return (
+            jsonify(
+                {
+                    "message": "Error al enviar el correo",
+                    "error": e,
+                }
+            ),
+            500,
+        )
+
+
 if __name__ == '__main__':
     print('server running')
     serve(app, host='0.0.0.0', port=5000)
